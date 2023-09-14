207f397d22b11fb7212e7bc3db5e42e761f36b6c
kingnsk <kingnsk2020@gmail.com>
2023-09-14 22:22:31 +0700

    first commit

---
+++ ClinicApp/EntityModels/PatientCard.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClinicApp.EntityModels
+{
+    public enum Gender {
+        [Description("Мужской")]
+        Male,
+        [Description("Женский")]
+        Female
+    };
+    public class PatientCard
+    {
+        public Int32 Id { get; set; }
+        public String Name { get; set; }
+        public Gender Gender { get; set; }
+        [Column(TypeName = "datetime2")]
+        public DateTime DateOfBirth { get; set; }
+        public Int32 Age { get; set; } 
+        public String Address { get; set; }
+        public String PhoneNumber { get; set; }
+        public List<Request> Requests { get; set; }
+    }
+}
---
+++ ClinicApp/Migrations/202309140923547_ConvertDateTimeToDatetime2.cs
@@ -0,0 +1,18 @@
+﻿namespace ClinicApp.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ConvertDateTimeToDatetime2 : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.PatientCards", "DateOfBirth", c => c.DateTime(nullable: false, precision: 7, storeType: "datetime2"));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.PatientCards", "DateOfBirth", c => c.DateTime(precision: 7, storeType: "datetime2"));
+        }
+    }
+}
